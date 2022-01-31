2232def407fd7651fd0a1fc3fb6dd0c16bcc0e8b
jannay11jr <jannaycaselles13@gmail.com>
2022-01-31 19:29:35 +0100

    Initial commit

---
+++ src/main/java/GC.java
@@ -0,0 +1,15 @@
+public class GC {
+    public static GC doStuff() {
+        GC newGC = new GC();
+        return newGC;
+    }
+
+    public static void main (String [] args) {
+        GC gc1;
+        GC gc2 = new GC();
+        GC gc3 = new GC();
+        GC gc4 = gc3;
+        gc1 = doStuff();
+        gc3 = null;
+    }
+}
